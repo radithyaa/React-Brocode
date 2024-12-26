637b14aface8c696d1130d05900ee0f263253c38
ahja <ahzathenation@gmail.com>
2024-12-26 13:00:15 +0700

    Course 3 BroCode, Props v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Card from "./Card";
-
+import Students from "./Student";
 export default function App() {
   return (
     <>
-      <Card />
+      <Students name="Ahza" age={16} isStudent={true} />
+      <Students name="Argie" age={18} isStudent={false} />
+      <Students name="Farel" age={17} isStudent={true} />
+      <Students />
     </>
   );
 }
